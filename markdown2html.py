a3133d58c82bfad149af433ef06a0dab5d67cdb8
skycode <sikiruyaya1@gmail.com>
2023-10-12 10:01:30 -0700

    convery markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
+
 """
-script that takes 2 arguments
+Convert Markdown to HTML
 """
+
+
 import sys
-from os import path
-import re
-
-
+import os
 if __name__ == "__main__":
-    """
-    a function that turns markdown to html 
-    """
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
-    elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
-        sys.stderr.write("Missing {}\n".format(sys.argv[1]))
-        sys.exit(1)
-    else:
-        html = []
-        with open(sys.argv[1], 'r') as file:
-            text = file.readlines()
-            text[-1] = text[-1].replace('\n', '')
 
-        with open(sys.argv[2], 'w') as file:
-            for string in text:
-                # convert (#) to html headings (h1 - h6)
-                count = string.count('#')
-                if count != 0:
-                    html_replace = string.replace('#' * count + ' ', '')
-                    html_replace = html_replace.replace('\n', '')
-                    html_line = "<h{}>{}</h{}>\n".format(
-                        count, html_replace, count)
-                    html.append(html_line)
-            file.writelines(html)
-            sys.exit(0)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.exists(input_file):
+        sys.stderr.write(f"Missing {input_file}")
+        sys.exit(1)
+
+    sys.exit(0)
