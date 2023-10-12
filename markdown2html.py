ec06067ced9afaee2c7acd326913f1e08af204f4
skycode <sikiruyaya1@gmail.com>
2023-10-12 07:40:06 -0700

    convert markdown to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
 """
-Markdown to HTML
+Convert Markdown to HTML
 """
 
 
 import sys
 from os import path
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 3:
-        message = "Usage: ./markdown2html.py README.md README.html"
-        sys.stderr.write("{}\n".format(message))
+        message = "Usage: ./markdown2html.py README.md README.html\n"
+        sys.stderr.write('{}'.format(message))
         sys.exit(1)
-
     input_file = sys.argv[1]
     output_file = sys.argv[2]
-
     if not path.exists(input_file):
-        sys.stderr.write(f"Missing {input_file}\n")
+        sys.stderr.write("Missing {}\n".format(input_file))
         sys.exit(1)
-
-    with open(input_file, mode='r') as md_file:
-        lines = md_file.readlines()
-
-    with open(output_file, mode='w') as html_file:
-        for line in lines:
-            if line.startswith('#'):
-                count = line.count('#')
-                content = line.strip('# ').strip()
-                html_header = f"<h{count}>{content}</h{count}>\n"
-                html_file.write(html_header)
-            else:
-                html_file.write(line)
 
     sys.exit(0)
